5ef0d8ded1245e2dc20c450165fd30bd72da4e60
David Tang <dtang859@gmail.com>
2016-06-15 14:37:34 -0700

    adds all adapter and serializer methods

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Ember Data Snippets for Atom
 ============================
 
-An Atom package containing snippets for all the Ember Data adapter and serializer methods.
+An Atom package containing snippets for all the Ember Data adapter and serializer methods so that you don't have to look up the method signature every time you want to override a method.
---
+++ create-snippet.js
@@ -0,0 +1,23 @@
+// A handy script for generating the CSON from the Ember API docs
+// Usage: node create-snippet.js
+// Run this and set to the "methods" variable:
+// $('.method h3.name.anchorable-toc').filter(function() { return !$(this).parent().hasClass('private'); }).map(function() { return $(this).find('code').text().trim() + $(this).next('.args').text().trim(); });
+
+var methods = ["extractAttributes(modelClass, resourceHash)", "extractErrors(store, typeClass, payload, id)", "extractId(modelClass, resourceHash)", "extractMeta(store, modelClass, payload)", "extractPolymorphicRelationship(relationshipType, relationshipHash, relationshipOptions)", "extractRelationship(relationshipModelName, relationshipHash)", "extractRelationships(modelClass, resourceHash)", "keyForAttribute(key, method)", "keyForLink(key, kind)", "keyForPolymorphicType(key, typeClass, method)", "keyForRelationship(key, typeClass, method)", "modelNameFromPayloadKey(key)", "normalize(modelClass, resourceHash, prop)", "normalizeArrayResponse(store, primaryModelClass, payload, id, requestType)", "normalizeCreateRecordResponse(store, primaryModelClass, payload, id, requestType)", "normalizeDeleteRecordResponse(store, primaryModelClass, payload, id, requestType)", "normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType)", "normalizeFindBelongsToResponse(store, primaryModelClass, payload, id, requestType)", "normalizeFindHasManyResponse(store, primaryModelClass, payload, id, requestType)", "normalizeFindManyResponse(store, primaryModelClass, payload, id, requestType)", "normalizeFindRecordResponse(store, primaryModelClass, payload, id, requestType)", "normalizeQueryRecordResponse(store, primaryModelClass, payload, id, requestType)", "normalizeQueryResponse(store, primaryModelClass, payload, id, requestType)", "normalizeResponse(store, primaryModelClass, payload, id, requestType)", "normalizeSaveResponse(store, primaryModelClass, payload, id, requestType)", "normalizeSingleResponse(store, primaryModelClass, payload, id, requestType)", "normalizeUpdateRecordResponse(store, primaryModelClass, payload, id, requestType)", "payloadKeyFromModelName(modelName)", "pushPayload(store, payload)", "serialize(snapshot, options)", "serializeAttribute(snapshot, json, key, attribute)", "serializeBelongsTo(snapshot, json, relationship)", "serializeHasMany(snapshot, json, relationship)", "serializeIntoHash(hash, typeClass, snapshot, options)", "serializePolymorphicType(snapshot, json, relationship)"];
+
+var snippet = methods.map(function(method) {
+  var methodName = method.substring(0, firstSpaceIndex);
+  var firstSpaceIndex = method.indexOf(' ');
+  var args = method.substring(firstSpaceIndex + 1);
+  var template = `
+    '#methodName':
+      'prefix': '#methodName'
+      'body': """
+        #methodName#args {
+          $1
+        }
+      """`;
+  return template.replace(/#methodName/g, methodName).replace(/#args/, args);
+});
+
+console.log(snippet.join(''));
